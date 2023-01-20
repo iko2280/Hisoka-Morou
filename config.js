9d1caafc343b6933474ff9493cbc997212385def
iko2280 <70009078+iko2280@users.noreply.github.com>
2023-01-20 21:27:30 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 /**
-   * Create By Dika Ardnt.
-   * Contact Me on wa.me/6288292024190
+   * Create By Iko DF.
+   * Contact Me on wa.me/6281935102005
    * Follow https://github.com/DikaArdnt
 */
 
@@ -14,15 +14,15 @@ global.APIs = {
 
 // Apikey Website Api
 global.APIKeys = {
-	'https://api.zahwazein.xyz': 'Your Key',
+	'https://api.zahwazein.xyz': 'is8eatS4',
 }
 
 // Other
-global.owner = ['6283167714830','62895336722950','6287721317870','6288292024190']
-global.premium = ['6288292024190']
-global.packname = 'Hisoka Morrow'
-global.author = 'WhatsApp Bot'
-global.sessionName = 'hisoka'
+global.owner = ['6281935102005']
+global.premium = ['6281935102005']
+global.packname = 'Aether'
+global.author = 'Iko DF'
+global.sessionName = 'Aether'
 global.prefa = ['','!','.','🐦','🐤','🗿']
 global.sp = '⭔'
 global.mess = {
@@ -38,7 +38,7 @@ global.mess = {
 }
 global.limitawal = {
     premium: "Infinity",
-    free: 100
+    free: 30
 }
 global.thumb = fs.readFileSync('./lib/hisoka.jpg')
 global.visoka = { url: 'https://telegra.ph/file/15209657f9d4f59c7ca1e.mp4' }
